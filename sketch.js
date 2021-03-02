482f95a783a82b2d99f8bfcd0e45a897918f0925
sonalimalviya08 <75033150+sonalimalviya08@users.noreply.github.com>
2021-03-02 10:47:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var canvas,bg;
+var together;
+var tom, tomImg1,tomImg2;
+var jerry, jerryImg1,jerryImg2;
+
+function preload() {
+    bg = loadImage("images/garden.png");
+    tomImg1= loadAnimation("images/tomOne.png");
+    tomImg2=loadAnimation("images/tomTwo.png","images/tomThree.png");
+    tomImg3= loadAnimation("images/tomFour.png");
+    jerryImg1=loadAnimation("images/jerryOne.png");
+    jerryImg2= loadAnimation("images/jerryTwo.png","images/jerryThree.png");
+    jerryImg3=loadAnimation("images/jerryFour.png");
+
+}
+
+function setup(){
+    canvas = createCanvas(1000,800);
+
+    tom = createSprite(870, 600);
+    tom.addAnimation("tomSleeping", tomImg1);
+    tom.scale = 0.2;
+
+    jerry = createSprite(200, 600);
+    jerry.addAnimation("jerryStanding", jerryImg1);
+    jerry.scale = 0.15;
+
+}
+
+function draw() {
+
+    background(bg);
+
+    if(tom.x - jerry.x < (tom.width - jerry.width)/2)
+    { //give velocity of tom to 0 and change animation
+       
+    }  
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+    if(keyCode === LEFT_ARROW){
+        //give velocity to tom as 0 to stop it
+       
+        //change animation when it touch the jerry
+        
+        //change animation of jerry also
+        
+        
+    }
+}
